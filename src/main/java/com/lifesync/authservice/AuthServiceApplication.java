74ace8d38eca73d8a8bb8c3b217d004d1a5f9a6b
ChiragMR27 <chiragmr676@gmail.com>
2026-08-15 20:48:15 +0530

    Switching to asyn

--- src/main/java/com/lifesync/authservice/AuthServiceApplication.java
+++ src/main/java/com/lifesync/authservice/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.lifesync.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync // Turns on background processing for the entire application
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
